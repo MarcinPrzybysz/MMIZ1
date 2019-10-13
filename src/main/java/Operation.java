80fbd7cd1143e1e177ed93d7bca7f5c1d1354a53
MarcinPrzybysz <example@example.com>
2019-10-13 17:39:11 +0200

    Representation, operations: add, substract, devide

---
+++ src/main/java/Operation.java
@@ -0,0 +1,21 @@
+package Entity;
+
+public class Operation {
+    //todo:dodawanie
+    //todo:odejmowanie
+    //todo:mnożenie
+    //todo:dzielenie
+
+    //todo:sprzeżenie
+    //todo:odwrotnosc
+
+    public ComplexNum add(ComplexNum x, ComplexNum y){
+
+
+
+        return null;
+    }
+
+
+
+}
---
+++ src/main/java/Representation.java
@@ -0,0 +1,19 @@
+package Entity;
+
+public class Representation {
+
+    public String printComplexNum(double img, double real){
+        return real+"+"+img+"i";
+    }
+
+    public void setNewComplex(){
+
+        
+
+        System.out.println("Podaj część rzczywistą");
+
+        System.out.println("Podaj część uruojoną");
+
+    }
+
+}
